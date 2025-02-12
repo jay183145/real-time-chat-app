545f7284de15c2abbf06610d6ffcc5d51259d2f5
kyle.lan <kylel@nfcc.cc>
2025-02-12 14:07:08 +0800

    feat: Add simple bottom nav

---
+++ components/bottom-nav/index.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Home, Activity, MessageCircle, User } from "lucide-react"
+import Link from "next/link"
+
+function BottomNav() {
+    return (
+        <div className="mt-auto border-t">
+            <div className="flex justify-around py-2">
+                {[
+                    { icon: Home, label: "首頁", href: "/" },
+                    { icon: Activity, label: "動態", href: "/activity" },
+                    { icon: MessageCircle, label: "聊天", href: "/chat" },
+                    { icon: User, label: "個人", href: "/profile" },
+                ].map((item) => (
+                    <Link href={item.href} key={item.label} className="flex flex-col items-center gap-1 px-4">
+                        <item.icon className="h-6 w-6" />
+                        <span className="text-xs">{item.label}</span>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default BottomNav
