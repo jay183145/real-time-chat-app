37e3487c793d70de2a5b9dc5f3f7897e61a978d8
kyle.lan <kylel@nfcc.cc>
2025-02-13 13:37:35 +0800

    feat: Add is isLoginConfirm method in auth modal store

--- lib/store/auth-modal.ts
+++ lib/store/auth-modal.ts
@@ -1,23 +1,27 @@
 import { create } from "zustand"
 
-type AuthModalType = "sign" | "login" | "forget" | "new-password" | null
+export type AuthModalType = "sign" | "login" | "forget" | "new-password" | null
 
 type AuthModalStore = {
     type: AuthModalType
     modalStack: AuthModalType[]
+    isLoginConfirm: boolean
     initAuthModal: (type: AuthModalType) => void
     openModal: (type: AuthModalType) => void
     closeModal: () => void
     closeAllModals: () => void
     backToPreviousModal: () => void
+    setIsLoginConfirm: (isLoginConfirm: boolean) => void
 }
 
 export const useAuthModalStore = create<AuthModalStore>((set, get) => ({
     type: null,
     modalStack: [],
+    isLoginConfirm: false,
     initAuthModal: (type: AuthModalType) => set({ type, modalStack: [] }),
     openModal: (type: AuthModalType) => set({ type, modalStack: [...get().modalStack, type] }),
     closeModal: () => set({ modalStack: get().modalStack.slice(0, -1) }),
     closeAllModals: () => set({ modalStack: [] }),
     backToPreviousModal: () => set({ modalStack: get().modalStack.slice(0, -1) }),
+    setIsLoginConfirm: (isLoginConfirm: boolean) => set({ isLoginConfirm }),
 }))
