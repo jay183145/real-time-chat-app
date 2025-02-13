671d05d81b2223fdef271720556f59f670b8d175
kyle.lan <kylel@nfcc.cc>
2025-02-14 00:12:23 +0800

    style: Adjust error modal style

--- components/modal/error-modal.tsx
+++ components/modal/error-modal.tsx
@@ -11,7 +11,7 @@ function ErrorModal({ error, isShow, setIsShow }: ErrorModalProps) {
     return (
         isShow && (
             <BaseModal onClick={() => setIsShow(false)}>
-                <div className="absolute left-1/2 top-1/2 flex w-full max-w-xs -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-2xl bg-neutral-200">
+                <div className="absolute left-1/2 top-1/2 flex w-full max-w-xs -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-2xl bg-neutral-700">
                     <div className="flex w-full items-center justify-between rounded-t-2xl bg-red-400 px-4 py-3">
                         <CircleX className="invisible text-xl" />
                         <span className="text-center text-xl font-bold">Error Occured!</span>
