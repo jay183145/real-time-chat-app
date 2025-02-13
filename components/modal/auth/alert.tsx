53ea3da28d93088f43230956e673cb4a3684d458
kyle.lan <kylel@nfcc.cc>
2025-02-13 22:56:32 +0800

    style: Adjust error modal and alert style

--- components/modal/auth/alert.tsx
+++ components/modal/auth/alert.tsx
@@ -2,7 +2,6 @@
 
 import { useAuthModalStore } from "@/lib/store/auth-modal"
 import React from "react"
-import { AlertCircle } from "lucide-react"
 import BaseModal from "@/components/modal"
 
 type AuthAlertProps = {
@@ -26,18 +25,17 @@ function AuthAlert({ isShow, setIsShow }: AuthAlertProps) {
         isShow && (
             <BaseModal>
                 <div className="fixed left-1/2 top-1/2 w-[200px] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-neutral-800">
-                    <div className="flex w-full items-center justify-center rounded-t-xl bg-warning-2 px-3 py-2 text-shades-100">
-                        <AlertCircle className="h-5 w-5" />
-                        <span className="pl-2">請先登入</span>
+                    <div className="flex w-full items-center justify-center rounded-t-xl bg-primary-3 px-3 py-2">
+                        <span className="font-semibold text-shades-100">請先登入</span>
                     </div>
-                    <div className="pb-4 pt-6 text-center text-sm">登入後即可加入聊天室</div>
+                    <div className="pb-4 pt-6 text-center">登入後馬上聊天</div>
                     <div className="h-px w-full bg-neutral-700" />
                     <div className="flex justify-center gap-2">
-                        <button className="w-full p-2 text-center text-xs text-primary-4" onClick={handleCancel}>
+                        <button className="w-full p-2 text-center text-sm text-primary-4" onClick={handleCancel}>
                             取消
                         </button>
                         <div className="w-px bg-neutral-600" />
-                        <button className="w-full p-2 text-center text-xs text-primary-4" onClick={handleConfirm}>
+                        <button className="w-full p-2 text-center text-sm text-primary-4" onClick={handleConfirm}>
                             確定
                         </button>
                     </div>
--- components/modal/error-modal.tsx
+++ components/modal/error-modal.tsx
@@ -15,11 +15,11 @@ function ErrorModal({ error, isShow, setIsShow }: ErrorModalProps) {
                     <div className="flex w-full items-center justify-between rounded-t-2xl bg-red-400 px-4 py-3">
                         <CircleX className="invisible text-xl" />
                         <span className="text-center text-xl font-bold">Error Occured!</span>
-                        <CircleAlert className="cursor-pointer text-xl" onClick={() => setIsShow(false)} />
+                        <CircleX className="cursor-pointer text-xl" onClick={() => setIsShow(false)} />
                     </div>
                     <div className="flex w-full flex-col items-center justify-center px-5 pb-8">
                         <div className="pb-3 pt-5">
-                            <CircleX className="text-4xl text-red-500" />
+                            <CircleAlert className="text-4xl text-red-500" />
                         </div>
                         <div className="text-md text-center font-semibold text-black">{error}</div>
                     </div>
