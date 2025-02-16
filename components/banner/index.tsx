3dce49fccd9989b0026a180faa5e55d9526b227b
kyle.lan <kylel@nfcc.cc>
2025-02-16 23:10:23 +0800

    feat: Make banner go to register auth modal

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -1,14 +1,22 @@
+"use client"
+
 import React from "react"
+import { useRouter } from "next/navigation"
 
 function Banner() {
+    const router = useRouter()
+
     return (
-        <div className="bg-primary-2 relative m-4 cursor-pointer rounded-xl p-4">
+        <div
+            onClick={() => router.push("/?auth=register")}
+            className="relative m-4 cursor-pointer rounded-xl bg-primary-2 p-4"
+        >
             <div className="mb-2 flex flex-col items-center gap-2 text-xl font-bold">
                 <span className="relative">
                     找尋新的交友圈
                     <span className="absolute -right-1 -top-1 h-2 w-2 rounded-full bg-blue-400" />
                 </span>
-                <span className="text-primary-5 text-center">立即註冊送 1000 積分</span>
+                <span className="text-center text-primary-5">立即註冊送 1000 積分</span>
                 <div className="text-sm opacity-80">時間: 2月10日 - 2月18日</div>
             </div>
         </div>
