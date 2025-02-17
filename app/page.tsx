88d3d3cdb89713f50fae06c2b4c3e97302a6e3cc
kyle.lan <kylel@nfcc.cc>
2025-02-17 09:40:42 +0800

    feat: Add Favorite section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import RootLayout from "@/components/layout"
 import Banner from "@/components/banner"
 import ChatList from "@/components/home/chat-list"
 // import HottestToday from "@/components/home/hottest-today"
-// import Favorite from "@/components/home/favorite"
+import Favorite from "@/components/home/favorite"
 
 export default function HomePage() {
     return (
@@ -11,11 +11,11 @@ export default function HomePage() {
             <Banner />
 
             {/* 聊天室區塊 */}
-            <div className="bg-primary-1 mx-4 mt-4 rounded-xl p-4">
+            <div className="mx-4 mt-4 rounded-xl bg-primary-1 p-4">
                 {/* 今日人氣排行 */}
                 {/* <HottestToday /> */}
                 {/* 收藏列表 */}
-                {/* <Favorite /> */}
+                <Favorite />
                 {/* 聊天室列表 */}
                 <ChatList />
             </div>
--- components/home/favorite.tsx
+++ components/home/favorite.tsx
@@ -1,15 +1,29 @@
-import React from "react"
+"use client"
+
+import cn from "@/utils/cn"
+import React, { useState } from "react"
 
 function Favorite() {
+    const [activeTab, setActiveTab] = useState("收藏")
+    const handleTabClick = (tab: string) => {
+        setActiveTab(tab)
+    }
     return (
         <div className="mb-4 flex justify-between">
             {[
                 { icon: "⭐", label: "收藏", color: "text-yellow-500" },
                 { icon: "🔥", label: "熱門", color: "text-red-500" },
                 { icon: "💝", label: "交友", color: "text-pink-500" },
-                { icon: "📻", label: "Podcast", color: "text-blue-500" },
+                { icon: "📻", label: "廣播", color: "text-blue-500" },
             ].map((item) => (
-                <button key={item.label} className="flex flex-col items-center gap-1">
+                <button
+                    key={item.label}
+                    className={cn(
+                        "flex items-center gap-1 px-3 py-1",
+                        activeTab === item.label && "rounded-3xl bg-neutral-800 shadow-lg",
+                    )}
+                    onClick={() => handleTabClick(item.label)}
+                >
                     <span className="text-xl">{item.icon}</span>
                     <span className={`text-sm ${item.color}`}>{item.label}</span>
                 </button>
