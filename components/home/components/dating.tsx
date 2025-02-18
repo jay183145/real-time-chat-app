fee249e58e0bdc0bf47973b082043da24a396207
kyle.lan <kylel@nfcc.cc>
2025-02-18 11:30:25 +0800

    feat: Add loading screen in chatlist

--- components/home/components/dating.tsx
+++ components/home/components/dating.tsx
@@ -10,15 +10,18 @@ import AuthAlert from "../../auth/alert"
 import cn from "@/utils/cn"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
+import Loading from "@/components/loading"
 
 dayjs.extend(relativeTime)
 
 function DatingChatList() {
     const [chatList, setChatList] = useState<Conversation[]>([])
     const { userInfo } = useAuthStore()
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         async function fetchChatList() {
+            setLoading(true)
             const { data, error } = await getConversations()
             if (error) {
                 console.error(error)
@@ -28,6 +31,7 @@ function DatingChatList() {
                 )
                 setChatList(favoriteChatList)
             }
+            setLoading(false)
         }
         fetchChatList()
     }, [userInfo?.user])
@@ -44,48 +48,56 @@ function DatingChatList() {
 
     return (
         <ul className="space-y-4">
-            {chatList.map((chat) => {
-                const participantsNames = chat.participants.map((p) => p.user).join(", ")
-                // 暫時改成需求的時間
-                // const participantNumber = `和其他${chat.participants.length > 3 ? ` ${chat.participants.length - 3} ` : ""}人也在線上`
-                const activeTime = dayjs(chat.timestamp).format("MM/DD HH:mm")
+            {loading && (
+                <div className="m-auto flex min-h-full w-full items-start justify-center pt-[20vh]">
+                    <Loading className="h-16 w-16" />
+                </div>
+            )}
+            {!loading &&
+                chatList.map((chat) => {
+                    const participantsNames = chat.participants.map((p) => p.user).join(", ")
+                    // 暫時改成需求的時間
+                    // const participantNumber = `和其他${chat.participants.length > 3 ? ` ${chat.participants.length - 3} ` : ""}人也在線上`
+                    const activeTime = dayjs(chat.timestamp).format("MM/DD HH:mm")
 
-                return (
-                    <li
-                        onClick={() => handleClickConversation(chat.id)}
-                        key={chat.id}
-                        className={cn(
-                            "flex cursor-pointer items-center justify-between rounded-lg bg-white p-4 shadow",
-                            "hover:motion-translate-x-out-[5%]",
-                        )}
-                    >
-                        <div className="flex w-[200px] flex-col items-start justify-start">
-                            <div className="flex w-[200px] items-center justify-between">
-                                <p className="truncate text-sm font-semibold text-neutral-200">{participantsNames}</p>
-                                <div className="flex items-center gap-1 text-nowrap px-2 text-xs font-semibold text-neutral-300">
-                                    <span>{activeTime}</span>
+                    return (
+                        <li
+                            onClick={() => handleClickConversation(chat.id)}
+                            key={chat.id}
+                            className={cn(
+                                "flex cursor-pointer items-center justify-between rounded-lg bg-white p-4 shadow",
+                                "hover:motion-translate-x-out-[5%]",
+                            )}
+                        >
+                            <div className="flex w-[200px] flex-col items-start justify-start">
+                                <div className="flex w-[200px] items-center justify-between">
+                                    <p className="truncate text-sm font-semibold text-neutral-200">
+                                        {participantsNames}
+                                    </p>
+                                    <div className="flex items-center gap-1 text-nowrap px-2 text-xs font-semibold text-neutral-300">
+                                        <span>{activeTime}</span>
+                                    </div>
                                 </div>
+                                <p className="w-[200px] truncate text-sm text-neutral-500">{chat.lastMessage}</p>
                             </div>
-                            <p className="w-[200px] truncate text-sm text-neutral-500">{chat.lastMessage}</p>
-                        </div>
-                        <div className="relative h-12 w-24">
-                            {/* eslint-disable-next-line @next/next/no-img-element */}
-                            <img
-                                src={chat.participants[0].avatar}
-                                alt={chat.participants[0].user}
-                                className="absolute left-0 top-0 z-10 h-12 min-h-[48px] w-12 min-w-[48px] rounded-full border-2 border-neutral-200"
-                            />
-                            {/* eslint-disable-next-line @next/next/no-img-element */}
-                            <img
-                                src={chat.participants[1].avatar}
-                                alt={chat.participants[1].user}
-                                className="absolute left-5 top-0 z-20 h-12 min-h-[48px] w-12 min-w-[48px] rounded-full border-2 border-neutral-200"
-                            />
-                            <ChevronRight className="absolute left-[68px] top-4 z-30 min-h-[12px] min-w-[12px] text-neutral-200" />
-                        </div>
-                    </li>
-                )
-            })}
+                            <div className="relative h-12 w-24">
+                                {/* eslint-disable-next-line @next/next/no-img-element */}
+                                <img
+                                    src={chat.participants[0].avatar}
+                                    alt={chat.participants[0].user}
+                                    className="absolute left-0 top-0 z-10 h-12 min-h-[48px] w-12 min-w-[48px] rounded-full border-2 border-neutral-200"
+                                />
+                                {/* eslint-disable-next-line @next/next/no-img-element */}
+                                <img
+                                    src={chat.participants[1].avatar}
+                                    alt={chat.participants[1].user}
+                                    className="absolute left-5 top-0 z-20 h-12 min-h-[48px] w-12 min-w-[48px] rounded-full border-2 border-neutral-200"
+                                />
+                                <ChevronRight className="absolute left-[68px] top-4 z-30 min-h-[12px] min-w-[12px] text-neutral-200" />
+                            </div>
+                        </li>
+                    )
+                })}
             <AuthAlert isShow={isShowAuthAlert} setIsShow={setIsShowAuthAlert} />
         </ul>
     )
